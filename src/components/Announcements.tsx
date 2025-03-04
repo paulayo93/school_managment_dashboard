5b92f95e59c7fb10096e103a02d5d35a152b2b0c
paulayo93 <52790286+paulayo93@users.noreply.github.com>
2025-03-04 13:46:58 +0100

    update announcement feature for admin
    update announcement implementation file

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,4 +1,6 @@
 
+
+const data = new Array(3).fill()
 const Announcements = () => {
     
     return (
@@ -10,15 +12,20 @@ const Announcements = () => {
         </div>  
 
         <div className="flex flex-col gap-4 mt-4">
-        
-        <div className="bg-lamaSkyLight rounded-md p-4">
+        {data.map((_, index) => 
+            
+ <div className="bg-lamaSkyLight rounded-md p-4" key={index}>
             <div className="flex items-center justify-between">
                 <h2 className="font-medium">Shcool Board Meeting </h2>
                 <span className="text-xs text-gray-400 bg-white rounded-md px-1">4th Aug, 2025</span>
             </div>
             <p className="text-sm text-gray-400 mt-1">This is the meeting with the board of the school </p>
         </div>
-        </div>
+
+        )
+
+        } 
+              </div>
 
         </div>
     );
